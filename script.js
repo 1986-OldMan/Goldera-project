c4ee78211fe0a15b693f80de6ef46124986f1b51
alexandru ivanescu <ivanescu.alexandru01@gmail.com>
2023-02-06 23:35:49 +0200

    change style of html ,css index . added navigation bar

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const GoldProductArray = [
+
+    "Gold" , "Gold coins" ,
+    "Gold bars" , "PAMP" ,
+    "Munze Osterreich" ,
+    "Argor-Heraeus" , "Valcambi Suisse" ,
+    "Rand Refinery" , "Perth Mint" ,
+    "Maple Leaf 1oz" , "Vienna Philharmonic",
+    "American Eagle" , "American Buffalo" , 
+    "Britannia 1 oz"
+];
+
+const searchInput = document.querySelector(".searchInput");
+
+const input = searchInput.querySelector("input");
+
+const resultBox = searchInput.querySelector(".resultBox");
+
+const icon = searchInput.querySelector(".icon");
+
+let linkTag = searchInput.querySelector("a");
+
+let webLink;
+
+input.onkeyup = (e) => {
+
+    let userData = e.target.value; 
+
+    let emptyArray = [];
+
+    if(userData){
+
+        emptyArray = GoldProductArray.filter((data)=>{
+            
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+        });
+        emptyArray = emptyArray.map((data)=>{
+      
+            return data = '<li>'+ data;
+        });
+        searchInput.classList.add("active"); 
+
+        showSuggestions(emptyArray);
+
+        let allList = resultBox.querySelectorAll("li");
+
+        for (let i = 0; i < allList.length; i++) {
+         
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    } else {
+        
+        searchInput.classList.remove("active");
+    }
+}
+
+function showSuggestions(list){
+
+    let listData;
+
+    if(!list.length){
+
+        userValue = inputBox.value;
+
+        listData = '<li>'+ userValue +'</li>';
+
+    } else {
+
+        listData = list.join('');
+    }
+    resultBox.innerHTML = listData;
+}
+
