0e4ef9179475e650110a0f9ba2fa8c0fa94301f1
alexandru ivanescu <ivanescu.alexandru01@gmail.com>
2023-05-30 20:48:31 +0300

    create a schema for products with mongoose

--- PRO/server.js
+++ PRO/server.js
@@ -5,6 +5,7 @@ dotenv.config({
 })
 const app = require('./app');
 
+//Connect the project to online database.
 const DB = process.env.DATABASE.replace('<PASSWORD>' , process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB , {
@@ -13,6 +14,27 @@ mongoose.connect(DB , {
     useFindAndModify: false,
     useUnifiedTopology: true
 }).then(() => {console.log('DB connection successful!')});
+
+//Create a schema for products
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String ,
+        required: [true , 'A product must have a name'] ,
+        unique: true
+    },
+
+    price: {
+        type: Number ,
+        require: [true , 'A product must have a name']
+    },
+
+    id: {
+        type: Number ,
+        require: [true , 'A product must have an id']
+    }
+});
+
+const Product = mongoose.model('Product' , productSchema);
 
 const port = process.env.PORT || 8000;
 app.listen(port , () => {
