ece8b73f2d2cd0f3dff0932d9c589d06a779b017
alexandru ivanescu <ivanescu.alexandru01@gmail.com>
2023-06-04 16:46:24 +0300

    change chema for database and tested after and add in products-simple.json new product

--- PRO/models/productModel.js
+++ PRO/models/productModel.js
@@ -3,6 +3,11 @@ const {Schema} = mongoose;
 
 //Create a schema for products
 const productSchema = new mongoose.Schema({
+
+    idProduct: {
+        type: Number ,
+    },
+
     name: {
         type: String ,
         required: [true , 'A product must have a name'] ,
@@ -14,14 +19,48 @@ const productSchema = new mongoose.Schema({
         required: [true , 'A product must have a name']
     },
 
-    id: {
+    description: {
+        type: String ,
+        required: [true , 'A product must have a description']
+    },
+    
+    manufacturer: {
+        type: String ,
+        required: [true , 'A product must have a manufacturer']
+    },
+
+    alloy: {
+        type: String ,
+        required:[true , 'A product must have type of alloy']
+    },
+
+    weight: {
+        type: Schema.Types.Mixed ,
+        required: [true , 'A product must have a weight']
+    },
+
+    fineness: {
         type: Number ,
-        required: [true , 'A product must have an id']
+        required: [true , 'A product must have fineness included']
+    },
+
+    dimensions: {
+        type: Schema.Types.Mixed ,
+        required: [true , 'A product must have dimensions included']
     },
 
     stock: {
-        type: Schema.Types.Mixed
-    }
+        type: Schema.Types.Mixed ,
+        required:[true , 'A product must have stock']
+    },
+
+    images: [String],
+
+    createdAt: {
+        type: Date ,
+        default: Date.now()
+    },
+
 });
 
 //Creating documents and testing the model(Schema)
