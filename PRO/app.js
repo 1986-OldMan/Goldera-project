7361bee4b57a7beb3d5cd9a7384f138d88ca12ed
alexandru ivanescu <ivanescu.alexandru01@gmail.com>
2023-07-09 19:56:29 +0300

    create a new middleware for better error handling for all project and modification on middleware for routes is not define

--- PRO/app.js
+++ PRO/app.js
@@ -26,14 +26,34 @@ app.use('/api/v1/users' , userRouter);
 /**
    * Middleware for the routes is not define.
    * .all is used to all HTTP methods (get , post , patch , delete and continue).
-   * the '*' it's used for the all URL's is catch
+   * the '*' it's used for the all URL's to catch.
+   * Creates a new Error object and sets the error.message property to the provided text message.
  */
 app.all('*' , (req , res , next) => {
-  res.status(404).json({
-    status: 'fail' ,
-    message: `Can't find ${req.originalUrl} on the server!`
-  })
-  next();
+  // res.status(404).json({
+  //   status: 'fail' ,
+  //   message: `Can't find ${req.originalUrl} on the server!`
+  // })
+
+  const err = new Error(`Can't find ${req.originalUrl} on the server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+/**
+ * Implementing better global Middleware for error handling.
+*/
+
+app.use((err , req , res , next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail'
+
+  res.status(err.statusCode).json({
+    status: err.status ,
+    message: err.message
+  });
 });
 
 module.exports = app;
