939ff25f8f61da559263bdde90c6ff53226285d3
alexandru ivanescu <ivanescu.alexandru01@gmail.com>
2023-07-16 09:12:16 +0300

    Handling invalid database id

--- PRO/controllers/errorController.js
+++ PRO/controllers/errorController.js
@@ -3,52 +3,62 @@
  * This middleware is exported and used in app.js.
  * error.stack property provides a string representation of the stack trace associated with an error object. It shows the sequence of function calls that led to the occurrence of the error, including the function names, file names, and line numbers.
  * implementing error during development(sendErrorDev) vs production(sendErrorProd).
- * in sendErrorDev = recived all detail , message of error and code of error.
+ * in sendErrorDev = recived all detail , message and code of error.
  * in sendErrorProd = in case error is operational you recived error code and error detail.
  *                  = in case error is not operation you recived error 500 and generic message.
  *                  = for more detail go to section appError.js in folder utils (PRO/utils/appError.js).
 */
 
-const sendErrorDev = (err , res) => {
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message , 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
     stack: err.stack
   });
-}
+};
 
-const sendErrorProd = (err , res) => {
-  // Operational , trusted error: send message to client.
-  if(err.isOperational) {
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     });
 
-    //Programming or other unknown error: don't leak error details.
+    // Programming or other unknown error: don't leak error details
   } else {
-    // 1) Log error.
-    console.error('Error 💥' , err);
+    // 1) Log error
+    console.error('ERROR 💥', err);
 
-    // 2) Send generic message.
+    // 2) Send generic message
     res.status(500).json({
       status: 'error',
       message: 'Something went wrong!'
-    })
+    });
   }
 };
 
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
 
-module.exports = (err , req , res , next) => {
-    // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
-
-    if(process.env.NODE_ENV === 'development') {
-      sendErrorDev(err , res);
-    } else if (process.env.NODE_ENV === 'production'){
-      sendErrorProd(err , res);
-    }
-  };
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = err;
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+    
+    console.log(error);
+  }
+};
